95fe14b71f588a6c6b31431a20851160f61dc0c4
Manuel Ortega <manuel@lasindias.coop>
2018-08-15 14:25:04 -0500

    Added quotes_controller

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,11 @@
 require "kadro"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+"..", "app",
+"controllers")
+
+require "quotes_controller"
+
 module BestQuotes
   class Application < Kadro::Application
   end
